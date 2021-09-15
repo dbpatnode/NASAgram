9353d57ce889f358503901b598bfc6234deb78c4
dbpatnodee <dbpatnode@hotmail.com>
2021-09-14 17:28:44 -0700

    basic header

--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Header.style.scss'
+import logo from './Antenna.png'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div class="header">
+            <div className="icons">
+                <img src={logo} alt="logo" ></img>
+                <a className="logo">NASAgram</a>
+            </div>
+        </div>
     );
 };
 
